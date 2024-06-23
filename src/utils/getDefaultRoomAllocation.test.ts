ef349e2ed1011cbc07319d64ec642c363bf5a01b
Victor <tubebigbig@gmail.com>
2024-06-23 15:13:31 +0800

    chore: adjust getDefaultRoomAllocation test file

--- src/utils/getDefaultRoomAllocation.test.ts
+++ src/utils/getDefaultRoomAllocation.test.ts
@@ -4,6 +4,46 @@ import getDefaultRoomAllocation, {
   Room,
 } from "./getDefaultRoomAllocation";
 
+const testDatas: {
+  people: People;
+  rooms: Room[];
+  expectPrice: number;
+}[] = [
+  {
+    people: { adult: 1, child: 1 },
+    rooms: [{ roomPrice: 100, adultPrice: 50, childPrice: 25, capacity: 2 }],
+    expectPrice: 175,
+  },
+  {
+    people: { adult: 4, child: 2 },
+    rooms: [
+      { roomPrice: 1000, adultPrice: 200, childPrice: 100, capacity: 4 },
+      { roomPrice: 0, adultPrice: 500, childPrice: 500, capacity: 4 },
+    ],
+    expectPrice: 2600,
+  },
+  {
+    people: { adult: 7, child: 3 },
+    rooms: [
+      { roomPrice: 2000, adultPrice: 400, childPrice: 200, capacity: 2 },
+      { roomPrice: 2000, adultPrice: 400, childPrice: 200, capacity: 2 },
+      { roomPrice: 2000, adultPrice: 200, childPrice: 100, capacity: 4 },
+      { roomPrice: 2000, adultPrice: 200, childPrice: 100, capacity: 4 },
+    ],
+    expectPrice: 8000,
+  },
+  {
+    people: { adult: 16, child: 0 },
+    rooms: [
+      { roomPrice: 500, adultPrice: 500, childPrice: 300, capacity: 4 },
+      { roomPrice: 500, adultPrice: 500, childPrice: 300, capacity: 4 },
+      { roomPrice: 0, adultPrice: 500, childPrice: 300, capacity: 8 },
+      { roomPrice: 500, adultPrice: 1000, childPrice: 600, capacity: 2 },
+    ],
+    expectPrice: 9000,
+  },
+];
+
 const getTotal = (roomAllocationList: RoomAllocation[]): number => {
   return roomAllocationList.reduce(
     (acc, roomAllocation) => acc + roomAllocation.price,
@@ -11,47 +51,11 @@ const getTotal = (roomAllocationList: RoomAllocation[]): number => {
   );
 };
 
-describe("should return room allocation", () => {
-  test("test 1", () => {
-    const people: People = { adult: 1, child: 1 };
-    const roomList: Room[] = [
-      { roomPrice: 100, adultPrice: 50, childPrice: 25, capacity: 2 },
-    ];
-    const result = getTotal(getDefaultRoomAllocation(people, roomList));
-    expect(result).toEqual(175);
-  });
-
-  test("test 2", () => {
-    const people: People = { adult: 4, child: 2 };
-    const roomList: Room[] = [
-      { roomPrice: 1000, adultPrice: 200, childPrice: 100, capacity: 4 },
-      { roomPrice: 0, adultPrice: 500, childPrice: 500, capacity: 4 },
-    ];
-    const result = getTotal(getDefaultRoomAllocation(people, roomList));
-    expect(result).toEqual(2600);
-  });
-
-  test("test 3", () => {
-    const people: People = { adult: 7, child: 3 };
-    const roomList: Room[] = [
-      { roomPrice: 2000, adultPrice: 400, childPrice: 200, capacity: 2 },
-      { roomPrice: 2000, adultPrice: 400, childPrice: 200, capacity: 2 },
-      { roomPrice: 2000, adultPrice: 200, childPrice: 100, capacity: 4 },
-      { roomPrice: 2000, adultPrice: 200, childPrice: 100, capacity: 4 },
-    ];
-    const result = getTotal(getDefaultRoomAllocation(people, roomList));
-    expect(result).toEqual(8000);
-  });
-
-  test("test 4", () => {
-    const people: People = { adult: 16, child: 0 };
-    const roomList: Room[] = [
-      { roomPrice: 500, adultPrice: 500, childPrice: 300, capacity: 4 },
-      { roomPrice: 500, adultPrice: 500, childPrice: 300, capacity: 4 },
-      { roomPrice: 0, adultPrice: 500, childPrice: 300, capacity: 8 },
-      { roomPrice: 500, adultPrice: 1000, childPrice: 600, capacity: 2 },
-    ];
-    const result = getTotal(getDefaultRoomAllocation(people, roomList));
-    expect(result).toEqual(9000);
+describe("find lowest room allocation price", () => {
+  testDatas.forEach(({ people, rooms, expectPrice }) => {
+    test(`test ${people.adult} adult and ${people.child} child`, () => {
+      const result = getTotal(getDefaultRoomAllocation(people, rooms));
+      expect(result).toEqual(expectPrice);
+    });
   });
 });
